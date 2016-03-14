5a0d42e147e199d21605a90bd2801b21aa496f76
maxim.syachin <MSyachin@luxoft.com>
2016-03-14 14:17:25 +0300

    - back to TomCat - instructions to fight slow startup in readme.MD

--- src/main/kotlin/ru/finnetrolle/telebot/restful/JerseyConfig.kt
+++ src/main/kotlin/ru/finnetrolle/telebot/restful/JerseyConfig.kt
@@ -2,19 +2,21 @@ package ru.finnetrolle.telebot.restful
 
 import org.glassfish.jersey.server.ResourceConfig
 import org.glassfish.jersey.server.ServerProperties
+import org.springframework.context.annotation.Configuration
 import org.springframework.stereotype.Component
 
 /**
  * Created by maxsyachin on 13.03.16.
  */
 @Component
+@Configuration
 open class JerseyConfig: ResourceConfig {
 
     constructor() {
-        packages(JerseyConfig::class.java.`package`.name)
-        register(com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider::class.java)
-        property(ServerProperties.RESPONSE_SET_STATUS_OVER_SEND_ERROR, true)
-        property(ServerProperties.WADL_FEATURE_DISABLE, true)
+        packages(ru.finnetrolle.telebot.restful.JerseyConfig::class.java.`package`.name)
+//        register(com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider::class.java)
+//        property(ServerProperties.RESPONSE_SET_STATUS_OVER_SEND_ERROR, true)
+//        property(ServerProperties.WADL_FEATURE_DISABLE, true)
     }
 
 }
