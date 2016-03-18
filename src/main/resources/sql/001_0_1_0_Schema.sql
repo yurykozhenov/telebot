7eb4ced1c82bb6c188e73832c6d9972f84b8fbf4
maxim.syachin <MSyachin@luxoft.com>
2016-03-18 09:28:52 +0300

    #13 - liquibase is enabled - some code cleaning

---
+++ src/main/resources/sql/001_0_1_0_Schema.sql
@@ -0,0 +1,18 @@
+BEGIN TRANSACTION;
+
+DROP TABLE IF EXISTS "pilots" CASCADE;
+
+CREATE TABLE "pilots" (
+    "id" INT PRIMARY KEY,
+    "first_name" VARCHAR(150),
+    "last_name" VARCHAR(150),
+    "username" VARCHAR(150),
+    "api_key" INTEGER NOT NULL,
+    "v_code" VARCHAR(50) NOT NULL,
+    "character_name" VARCHAR(100) NOT NULL,
+    "character_id" BIGINT NOT NULL,
+    "moderator" BOOLEAN DEFAULT FALSE,
+    "renegade" BOOLEAN DEFAULT FALSE
+);
+
+END TRANSACTION;
