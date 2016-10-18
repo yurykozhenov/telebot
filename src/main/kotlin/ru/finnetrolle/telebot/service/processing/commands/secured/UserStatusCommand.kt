45ae36eaff7bcfa002c5ef17278acd42d4544877
maxim.syachin <MSyachin@luxoft.com>
2016-10-18 15:49:02 +0300

    0.3.7.4 - add /us command

---
+++ src/main/kotlin/ru/finnetrolle/telebot/service/processing/commands/secured/UserStatusCommand.kt
@@ -0,0 +1,41 @@
+package ru.finnetrolle.telebot.service.processing.commands.secured
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+import ru.finnetrolle.telebot.model.Pilot
+import ru.finnetrolle.telebot.model.PilotRepository
+import ru.finnetrolle.telebot.service.internal.UserService
+import ru.finnetrolle.telebot.util.MessageLocalization
+
+/**
+ * Telegram bot
+ * Licence: MIT
+ * Author: Finne Trolle
+ */
+@Component
+class UserStatusCommand : AbstractSecuredCommand() {
+
+    @Autowired
+    private lateinit var loc: MessageLocalization
+
+    @Autowired
+    private lateinit var userService: UserService
+
+    @Autowired
+    private lateinit var repo: PilotRepository
+
+    override fun name() = "/US"
+
+    override fun description() = loc.getMessage("telebot.command.description.us")
+
+    override fun execute(pilot: Pilot, data: String): String {
+        val user = repo.findByCharacterName(data)
+        if (user == null) {
+            return loc.getMessage("messages.user.not.found")
+        } else {
+            val moder = if (user.moderator) " is moderator" else ""
+            val renegade = if (user.renegade) " is renegade" else ""
+            return "${user.characterName}$moder$renegade"
+        }
+    }
+}
