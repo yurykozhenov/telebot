8f06179de62a924e965fc90a7fd39f75616e79f4
maxim.syachin <MSyachin@luxoft.com>
2017-02-21 23:48:46 +0300

    0.4.7 new universal list command

---
+++ src/main/kotlin/ru/finnetrolle/telebot/service/processing/commands/secured/ListCommand.kt
@@ -0,0 +1,61 @@
+package ru.finnetrolle.telebot.service.processing.commands.secured
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+import ru.finnetrolle.telebot.model.*
+import ru.finnetrolle.telebot.util.MessageLocalization
+
+/**
+ * Telegram bot
+ * Licence: MIT
+ * Author: Finne Trolle
+ */
+@Component
+class ListCommand : AbstractSecuredCommand() {
+
+    @Autowired
+    private lateinit var loc: MessageLocalization
+
+    @Autowired
+    private lateinit var pilots: PilotRepository
+
+    @Autowired
+    private lateinit var allies: AllianceRepository
+
+    @Autowired
+    private lateinit var corps: CorporationRepository
+
+    override fun name() = "/LIST"
+
+    override fun description() = loc.getMessage("telebot.command.description.list")
+
+    override fun execute(pilot: Pilot, data: String): String {
+        val list: List<String> = when (data.toUpperCase()) {
+            "PILOTS" -> {
+                pilots.findByRenegadeFalse().pilotNames()
+            }
+            "MODERATORS" -> {
+                pilots.findByModeratorTrue().pilotNames()
+            }
+            "SPEAKERS" -> {
+                pilots.findBySpeakerTrue().pilotNames()
+            }
+            "ALLIANCES" -> {
+                allies.findAll().allyNames()
+            }
+            "CORPORATIONS" -> {
+                corps.findAll().corpNames()
+            }
+            else -> listOf()
+        }
+        if (list.isEmpty()) {
+            return loc.getMessage("messages.list.nothing")
+        }
+        return loc.getMessage("messages.list.${data.toUpperCase()}", list.size, list)
+    }
+
+    fun List<Pilot>.pilotNames() = this.map { it.characterName }.sortedBy { it.toUpperCase() }
+    fun List<Alliance>.allyNames() = this.sortedBy { it.title.toUpperCase() }.map { "[${it.ticker}] - ${it.title}" }
+    fun List<Corporation>.corpNames() = this.sortedBy { it.title.toUpperCase() }.map { "[${it.ticker}] - ${it.title}" }
+
+}
